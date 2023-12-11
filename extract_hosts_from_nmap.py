ae755f203a28d4364e0c9b087d48da9d10424445
Martin Schobert <martin@schobert.cc>
2023-12-11 15:55:13 +0100

    Fix: select web-ports, better error messages.

--- extract_hosts_from_nmap.py
+++ extract_hosts_from_nmap.py
@@ -16,11 +16,12 @@ def parse_scan_report(nmap_xml_files, port):
         try:
 
             nmap_report = NmapParser.parse_fromfile(nmap_xml_file)
+            
             for host in nmap_report.hosts:
                 for service in host.services:
+                    
                     if service.state == 'open':
-
-                        if port == 'web':
+                        if port == 'web' or port == 'www':
                             if 'https' in service.service:
                                 targets.append("https://%s:%s" % (host.address, service.port))
 
@@ -31,11 +32,11 @@ def parse_scan_report(nmap_xml_files, port):
                                 targets.append("http://%s:%s" % (host.address, service.port))
                                 
                         else:
-                            if int(port) == int(service.port):
+                            if port == int(service.port):
                                 targets.append(host.address)
                             
-        except NmapParserException:
-            #print(f"File {nmap_xml_file} is not yet ready.")
+        except NmapParserException as e:
+            print(f"File {nmap_xml_file} is not yet ready:" + str(e), file=sys.stderr)
             pass
             
     return targets
@@ -48,7 +49,7 @@ def main():
     # process command line arguments
     parser = argparse.ArgumentParser(description='Extract hosts from an Nmap XML file')
     parser.add_argument('--xml', nargs='+', help='Nmap XML file to parse', metavar='FILE')
-    parser.add_argument('--port', help='Which port to lookup? Special port "www" matches any web related port.', metavar='PORT')
+    parser.add_argument('--port', help='Which port to lookup? Special port "www" or "web" matches any web related port.', metavar='PORT')
     (options, args) = parser.parse_known_args()
 
     if options.xml:
